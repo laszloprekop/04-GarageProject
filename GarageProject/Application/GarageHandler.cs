5a5b919692631f4c0424d2a85140d8ae0ae4fb46
Laszlo Prekop <laszlo.prekop@gmail.com>
2026-05-07 21:50:18 +0200

    feat(application): add GarageHandler class with basic structure and TODOs

---
+++ GarageProject/Application/GarageHandler.cs
@@ -0,0 +1,20 @@
+using GarageProject.Domain;
+
+namespace GarageProject.Application;
+
+public class GarageHandler
+{
+    private Garage<Vehicle>? _garage;
+    
+    public int Capacity => _garage?.Capacity ?? 0;
+    
+    public void CreateGarage(int capacity) =>
+        _garage = new Garage<Vehicle>(capacity);
+    
+    // TODO - add parking
+    // TODO - add retrieving
+    // TODO - add get all vehicles
+    // TODO - add get by registration number, type, color, number of wheels, fuel type
+    
+    // TODO - mock data?
+}
