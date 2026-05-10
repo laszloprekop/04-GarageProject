95ce94c5bcb6f326b7ed961501f7b6b05e2e97c7
Laszlo Prekop <laszlo.prekop@gmail.com>
2026-05-10 14:53:24 +0200

    prep(domain, application): implement vehicle parking in Garage and add Populate method

--- GarageProject/Application/GarageHandler.cs
+++ GarageProject/Application/GarageHandler.cs
@@ -5,20 +5,27 @@ namespace GarageProject.Application;
 public class GarageHandler
 {
     private Garage<Vehicle>? _garage;
-    
+
     public int Capacity => _garage?.Capacity ?? 0;
-    
+
     public void CreateGarage(int capacity) =>
         _garage = new Garage<Vehicle>(capacity);
+
+    private bool ParkVehicle(Vehicle vehicle) =>
+        _garage?.Add(vehicle) ?? false;
     
-    // TODO - add parking
     // TODO - add retrieving
     // TODO - add get all vehicles
     // TODO - add get by registration number, type, color, number of wheels, fuel type
-    
+
     // TODO - mock data?
     public void Populate()
     {
-        throw new NotImplementedException();
+        ParkVehicle(new Car("ABC123", "Red", 4, FuelType.Gasoline));
+        ParkVehicle(new Car("DEF456", "Green", 4, FuelType.Diesel));
+        ParkVehicle(new Car("GHI789", "Blue", 4, FuelType.Electric));
+        // ParkVehicle(new Motorcycle("XYZ789", "Blue", 2, FuelType.Diesel));
+        // ParkVehicle(new Motorcycle("PQR012", "Red", 2, FuelType.Gasoline));
+        // ParkVehicle(new Boat("JKL321", "Red", 4, FuelType.Diesel));
     }
 }
--- GarageProject/Domain/Garage.cs
+++ GarageProject/Domain/Garage.cs
@@ -5,10 +5,29 @@ public class Garage<T> where T : Vehicle
     private readonly T?[] _vehicles;
     public int Capacity { get; }
     public int Count => _vehicles.Count(v => v is not null);
-    
+
     public Garage(int capacity)
     {
         Capacity = capacity;
         _vehicles = new T?[capacity];
+    }
+
+    public bool Add(T vehicle)
+    {
+        if (Count >= Capacity) return false;
+        if (_vehicles.Any(v =>
+                v?.RegistrationNumber.Equals(vehicle.RegistrationNumber, StringComparison.OrdinalIgnoreCase) == true))
+            return false;
+
+        for (var i = 0; i < _vehicles.Length; i++)
+        {
+            if (_vehicles[i] is null)
+            {
+                _vehicles[i] = vehicle;
+                return true;
+            }
+        }
+
+        return false;
     }
 }
