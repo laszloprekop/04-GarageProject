b183e3afc1deaca488a2d0963fcf9c5949f4da82
Laszlo Prekop <laszlo.prekop@gmail.com>
2026-05-11 16:27:59 +0200

    prep(domain): add polymorphic serialization support for Vehicle subclasses

--- GarageProject/Domain/Vehicle.cs
+++ GarageProject/Domain/Vehicle.cs
@@ -1,4 +1,17 @@
+using System.Text.Json.Serialization;
+
 namespace GarageProject.Domain;
+
+/* I have to admit, this polymorphic sorcery here is pasted and customized code - I just really wanted the data persistence to work as a stretch goal.
+    I will definitely have to come back and fully understand what's going on here'.
+ */
+
+[JsonPolymorphic(TypeDiscriminatorPropertyName = "$type")]
+[JsonDerivedType(typeof(Car), "car")]
+[JsonDerivedType(typeof(Motorcycle), "motorcycle")]
+[JsonDerivedType(typeof(Bus), "bus")]
+[JsonDerivedType(typeof(Airplane), "airplane")]
+[JsonDerivedType(typeof(Boat), "boat")]
 
 public abstract class Vehicle
 {
