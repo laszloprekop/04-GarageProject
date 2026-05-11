f51c36c19dcbd1560bd1032ac724a0faba5bb86a
Laszlo Prekop <laszlo.prekop@gmail.com>
2026-05-11 10:04:44 +0200

    refactor(app): update application lifecycle calls to avoid using obsolete methods

--- GarageProject/Program.cs
+++ GarageProject/Program.cs
@@ -1,6 +1,5 @@
 ﻿using Terminal.Gui.App;
 using GarageProject.UI;
 
-Application.Init();
-Application.Run(new GarageApp());
-Application.Shutdown();
+using var app = Application.Create().Init();
+app.Run(new GarageApp());
